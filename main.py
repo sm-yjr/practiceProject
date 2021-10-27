ce3ff0860219327969a3d6117b2dcb29ea246ac2
yangjiarui <245837941@qq.com>
2021-10-27 16:53:50 +0800

    1.0.1 hello world! test

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from select_sort import selection_sort
 
+
+def test():
+    print("hello world!")
+
+
 # test for binary search
 # my_list = [1, 3, 5, 5, 7, 9]
 # print(binary_search(my_list, 5))
 # print(binary_search(my_list, 5))
 
 # test for select sort
+test()
 print(selection_sort([5, 3, 6, 2, 10]))
 print(selection_sort([3, 2, 7, 9, 1]))
