0774fdc63506354ca633c085b5531278483b7b94
yangjiarui <245837941@qq.com>
2021-11-01 13:37:27 +0800

    1.1.2 Greedy Algorithm Amendment Add some annotations.

--- greedy.py
+++ greedy.py
@@ -1,3 +1,4 @@
+# () for tuple, [] for list, {} for set & hash table
 states_needed = {'mt', 'wa', 'or', 'id', 'nv', 'ut', 'ca', 'az'}
 stations = {'kone': {'id', 'nv', 'ut'}, 'ktwo': {'wa', 'id', 'mt'}, 'kthree': {'or', 'nv', 'ca'},
             'kfour': {'nv', 'ut'}, 'kfive': {'ca', 'az'}}
@@ -5,6 +6,7 @@ final_stations = set()
 while states_needed:
     best_station = None
     states_covered = set()
+    # for key, value in dict.items()
     for station, states_for_station in stations.items():
         covered = states_needed & states_for_station
         if len(covered) > len(states_covered):
